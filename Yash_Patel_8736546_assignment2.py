936fabca7819d8b875ce58b0471063e7755cd205
Yash Patel <ypatel6546@conestogac.on.ca>
2021-10-23 14:17:28 -0400

    1st commit

---
+++ Yash_Patel_8736546_assignment2.py
@@ -0,0 +1,8 @@
+print("------------------WELCOME TO OUR SHOP--------------------")
+
+age = input('''What is your age? ''')
+print("Hi, welcome to my shop. Here are items available")
+
+shirt_price = 19.99
+short_price = 8.95
+
